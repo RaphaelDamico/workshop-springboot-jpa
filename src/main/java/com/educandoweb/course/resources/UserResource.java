4052c16670453953657a20f760d940b7037ca941
RaphaelDamico <raphaelvivarinid@gmail.com>
2024-05-10 16:11:04 -0300

    User delete

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,22 +24,36 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	
+	//Este método recebe uma requisição GET, busca todos os usuários através
+	//de um serviço e retorna essa lista como resposta HTTP com status 200 (OK).
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
 		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
+	//Este método permite que os clientes recuperem um usuário específico
+	//enviando uma solicitação GET para o URL apropriado com o ID do usuário.
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	//Este método de controlador lida com a criação de um novo usuário,
+	//persiste os dados por meio de um serviço, e retorna uma resposta
+	//informando ao cliente que o recurso foi criado com sucesso e fornecendo sua localização.
 	@PostMapping
 	public ResponseEntity<User> insert(@RequestBody User obj) {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
 	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
